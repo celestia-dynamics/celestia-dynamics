65684d7b6db17e0c717d9ee5ef88dac4b1138852
parthbhanti22 <parthbhanti22@gmail.com>
2025-07-25 02:08:41 +0530

    Phase 2.4 with errors

--- celestia-backend/main.go
+++ celestia-backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"celestia-backend/config"
+	"celestia-backend/routes"
 	"log"
 	"net/http"
 
@@ -11,6 +12,7 @@ import (
 func main(){
 
 	config.ConnectDB()
+
 	router := gin.Default()
 
 	router.GET("/",func(c *gin.Context){
@@ -19,12 +21,17 @@ func main(){
 		})
 	})
 
+	routes.AuthRoutes(router)
+
+	log.Println("Server started on http://localhost:8080")
+	router.Run(":8080");
+
 	//TODO: Plug in routes here
 	//Example: authRoutes(router)
 
-	log.Println("Starting server on: 8080...")
-	err := router.Run(":8080")
-	if err!= nil{
-		log.Fatal("Server couldn't start:", err)
-	}
+	//log.Println("Starting server on: 8080...")
+	//err := router.Run(":8080")
+	//if err!= nil{
+	//	log.Fatal("Server couldn't start:", err)
+	//}
 }
